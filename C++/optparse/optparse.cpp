ca43def9dc7ade2c0aab2e0f513bef5d67f6af4a
psholtz <paul.sholtz@gmail.com>
2011-03-15 23:43:59 -0700

    added a comment

--- C++/optparse/optparse.cpp
+++ C++/optparse/optparse.cpp
@@ -4,6 +4,23 @@
 
 using namespace std;
 
+/***************************************************************
+ * Designed to provide services similar to string.split() 
+ * available on platforms like Ruby or Python.
+ *  
+ *  vector<string> a = OptParser::split("this:that",':');
+ * 
+ * will return the vector {"this","that"}.
+ *
+ * This method does not skip empty tokens, so the following
+ * will return four items, one of which is empty:
+ * 
+ *  vector<string> a = OptParser::split("one:two::three",':');
+ *
+ * @Parameter: s - string to parse
+ * @Parameter: delim - character token to split on
+ * @Returns: vector compromised of string parsed on token
+ ***************************************************************/
 vector<string>
 OptParser::split(const string &s, char delim)
 {
