a6c1717da1e1fa9f89a5ef19b97b55e333b8c96c
psholtz <paul.sholtz@gmail.com>
2011-03-16 10:26:53 -0700

    added more methods

--- C++/optparse/optparse.h
+++ C++/optparse/optparse.h
@@ -7,9 +7,18 @@ public:
 	OptParser(int argc, char* argv[]);
 	~OptParser();
 
+	//
+	// Attribute management.
+	//
 	void prepare_to_start_attributes();
+	void add_integer_attribute();
+	void add_string_attribute();
 	void prepare_to_end_attributes();
 
+	//
+	// "Main" methods of this class.
+	//
+	void parse();
 	void usage();
 
 	static std::vector<std::string> split(const std::string &s, char delim);
