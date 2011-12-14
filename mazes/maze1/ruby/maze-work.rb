345ce5b7c34914ae08092eb537044aa931227493
psholtz <paul.sholtz@gmail.com>
2011-12-13 16:09:02 -0800

    adding animation support

---
+++ mazes/maze1/ruby/maze-work.rb
@@ -0,0 +1,212 @@
+#!/usr/bin/ruby
+
+require 'optparse'
+
+DEFAULT_WIDTH = 10
+DEFAULT_HEIGHT = 10
+DEFAULT_SEED = rand(0xFFFF_FFFF)
+DEFAULT_ANIMATE = false
+DEFAULT_DELAY = 0.04
+
+# ===================================================================
+# Class Maze defines basic behavior to which a maze should conform.
+# It provides basic initialization/construction for the maze class, 
+# and provides a method for drawing ASCII mazes. 
+#
+# Specific "maze-carving" techniques are implemented in subclasses.
+# ====================================================================
+class Maze
+	# +++++++++++++++++++++++++++
+	# Configure class variables.
+	# +++++++++++++++++++++++++++
+	@@N, @@S, @@E, @@W = 1, 2, 4, 8 
+	@@DX = { @@E => 1, @@W => -1, @@N => 0, @@S => 0 }
+	@@DY = { @@E => 0, @@W => 0, @@N => -1, @@S => 1 }
+	@@OPPOSITE = { @@E => @@W, @@W => @@E, @@N => @@S, @@S => @@N }
+
+	def self.N; @@N; end
+	def self.S; @@S; end
+	def self.E; @@E; end
+	def self.W; @@W; end
+	def self.DX; @@DX; end
+	def self.DY; @@DY; end
+	def self.OPPOSITE; @@OPPOSITE; end
+	
+	# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	# Initialize a new 2D maze with the given width and height.
+	#
+	# Default seed value will give "random" behavior.
+	# User-supplied seed value will give "deterministic behavior.
+	# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	def initialize( w=DEFAULT_WIDTH, h=DEFAULT_HEIGHT, s=DEFAULT_SEED )
+		@width = w
+		@height = h
+		@seed = s
+
+		srand(@seed)	
+
+		@grid = Array.new(h) { Array.new(w,0) }
+	end
+	
+	attr_reader :width, :height, :seed
+
+	# +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	# Draw the grid, starting in the upper-left hand corner.
+	# +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	def draw
+		#
+		# Draw the "top" line.
+		#
+		puts " " + "_" * (@width * 2 - 1)
+	
+		#	
+		# Draw each of the rows.
+		#
+		@height.times do |y|
+			print "|"
+			@width.times do |x|
+				# render "bottom" using "S" switch
+				print( (@grid[y][x] & @@S != 0) ? " " : "_" )
+			
+				# render "side" using "E" switch	
+				if @grid[y][x] & @@E != 0
+					print( ( (@grid[y][x] | @grid[y][x+1]) & @@S != 0 ) ? " " : "_" )
+				else
+					print "|"
+				end
+			end
+			puts
+		end
+
+		#	
+		# Output maze metadata.
+		#
+		puts "#{$0} #{@width} #{@height}  #{@seed}"
+	end
+end
+
+# =========================================================================
+# Class BackTracker implements a simple recursive back-tracking algorithm
+# to draw ASCII mazes. The algorithms works as a "depth-first" search
+# of the "tree" or "graph" representing the maze.
+# 
+# A possible optimization might be to implement a "breadth-first" search.
+# =========================================================================
+class BackTracker < Maze
+
+	# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	# Initialize a new 2D maze with the given width and height.
+	#
+	# Default seed value will give "random" behavior.
+	# User-supplied seed value will give "deterministic behavior.
+	# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	def initialize( w=DEFAULT_WIDTH, h=DEFAULT_HEIGHT, s=DEFAULT_SEED, a=DEFAULT_ANIMATE, d=DEFAULT_DELAY )
+		#
+		# Invoke super-constructor
+		#
+		super(w,h,s)
+
+		# 
+		# Only prepare the maze beforehand if we are doing "static" (i.e., animate=false) drawing
+		#
+		@delay = d
+		@animate = a
+		if not @animate
+		      carve_passage_from(0,0)
+		end
+	end
+
+	# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	# Recursively carve passages through the maze, starting at (x,y).
+	#
+	# Algorithm halts when all "cells" in the maze have been visited.
+	# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	def carve_passage_from(x,y)
+		directions = [@@N,@@S,@@E,@@W].sort_by{rand}
+		directions.each do |direction|
+			#
+			# Render updates of the maze on a "cell-by-cell" basis
+			#
+			if @animate
+			      draw(update=true)
+			      sleep @delay
+			end
+
+			dx,dy = x + @@DX[direction], y + @@DY[direction]
+			if dy.between?(0,@grid.length-1) && dx.between?(0,@grid[dy].length-1) && @grid[dy][dx] == 0
+				@grid[y][x] |= direction			# "open" the wall from current cell;
+				@grid[dy][dx] |= @@OPPOSITE[direction]		# "open" the wall from adjacent cell;
+				carve_passage_from(dx,dy)
+			end
+		end
+
+		# 
+		# Make one final call to "update" to display last cell
+		#
+		if @animate
+		      draw(update=true)
+		end
+	end
+
+	# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
+	# Method only needs to be overridden if we are animating.
+	# 
+	# If we are drawing the maze statically, defer to the superclass.
+	# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
+	def draw(update=false)
+	    if update or not @animate
+	        print "\e[H"
+		if not @animate; print "\e[2J"; end
+		super()
+	    else
+		print "\e[2J"
+		carve_passage_from(0,0)
+	    end
+	end
+
+	protected :carve_passage_from
+end
+
+# ============================
+# Command line code goes here
+# ============================
+OPTIONS  = {
+	:w => DEFAULT_WIDTH,
+	:h => DEFAULT_HEIGHT,
+	:s => DEFAULT_SEED,
+	:a => DEFAULT_ANIMATE,
+	:d => DEFAULT_DELAY
+}
+
+if __FILE__ == $0
+	ARGV.options do |o|
+		# parse the command line options
+		o.separator ""
+		o.on("-w","--width=[value]", Integer, "Width of maze (default: " + DEFAULT_WIDTH.to_s + ")") 		{ |OPTIONS[:w]| }
+		o.on("-h","--height=[value]", Integer, "Height of maze (default: " + DEFAULT_HEIGHT.to_s+ ")")		{ |OPTIONS[:h]| }
+		o.on("-s","--seed=[value]", Integer, "User-defined seed will model deterministic behavior (default: " + DEFAULT_SEED.to_s + ")")	{ |OPTIONS[:s]| }
+		o.on("-a","--[no-]animated", true.class, "Animate rendering (default: " + DEFAULT_ANIMATE.to_s + ")") 	{ |OPTIONS[:a]| }
+		o.on("-d","--delay=[value]", Float, "Animation delay (default: " + DEFAULT_DELAY.to_s + ")")   	 	{ |OPTIONS[:d]| }
+		o.separator ""
+		o.parse!
+
+		# sanitize the input from the command line
+		good = true
+		if OPTIONS[:w] == "" or OPTIONS[:w] == nil 
+			good = false
+		elsif OPTIONS[:h] == "" or OPTIONS[:h] == nil 
+			good = false
+		elsif OPTIONS[:s] == "" or OPTIONS[:s] == nil
+			good = false
+		elsif OPTIONS[:d] == "" or OPTIONS[:d] == nil
+		        good = false
+  		end
+
+		if good
+			# build and draw a new back-tracking maze
+			BackTracker.new(w=OPTIONS[:w], h=OPTIONS[:h], s=OPTIONS[:s], a=OPTIONS[:a], d=OPTIONS[:d]).draw
+		else
+			puts o
+		end
+	end
+end
