5632f0d11b36b21699d5b6a5ef1c29b8f108d0b4
psholtz <paul.sholtz@gmail.com>
2012-03-08 12:29:21 -0800

    updating the cipher

--- ciphers/solitaire/ruby/cipher.rb
+++ ciphers/solitaire/ruby/cipher.rb
@@ -32,7 +32,7 @@ class String
     # ================================== 
     def sanitize!(n=5)
     	self.upcase!
-	self.sgub!(/[^A-Z]/,"")
+	self.gsub!(/[^A-Z]/,"")
 	self.pad!(n)
     end
 
@@ -97,6 +97,7 @@ class Cipher
 	s.bytes.to_a.zip(stream.bytes.to_a) { |x|
 	    out << m.call(x[0],x[1]).chr
 	}
+	out
     end
 
     protected :combine
@@ -131,10 +132,106 @@ class SolitaireKeyStream < KeyStream
     attr_accessor :add, :sub
 
     def inspect; "SolitaireKeyStream"; end
+
+    # ++++++++++++++++++++++++++++
+    # Abstract method definition. 
+    # ++++++++++++++++++++++++++++
+    def generate_keystream(length)
+    	@deck = Deck.new
+	result = []
+	while result.length != length
+	    @deck.move_A
+	    @deck.move_B
+	    @deck.triple_cut
+	    @deck.count_cut
+	    letter = @deck.output_letter
+	    result << letter unless letter.nil?
+	end
+	result.join
+    end
 end
 
+# =========================================================
+# Class Deck implements a standard 52-card deck of cards.
+#
+# There are two Jokers, designated by "A" and "B".
+# =========================================================
 class Deck
+    # ++++++++++++++++++++++++++++++++++++
+    # Initialize with a key, the "secret"
+    # ++++++++++++++++++++++++++++++++++++
+    def initialize(key=(1..52).to_a << "A" << "B")
+        @deck = key
+    end
 
+    def inspect; "Deck"; end
+
+    # ++++++++++++++++++++++++++++
+    # M O V E   C A R D   A P I s
+    # ++++++++++++++++++++++++++++ 
+    def move_A
+        move_down( @deck.index("A") )
+    end
+
+    def move_B
+        2.times { move_down( @deck.index("B") ) }
+    end
+
+    def move_down(index)
+        if index == @deck.length - 1
+  	    @deck[1..1] = @deck[index], @deck[1]
+	    @deck.pop
+	else
+	    @deck[index], @deck[index+1] = @deck[index+1], @deck[index]
+	end
+    end
+
+    # ++++++++++++++++++++++++++++++++++ 
+    # C A R D   C U T T I N G   A P I s
+    # ++++++++++++++++++++++++++++++++++
+    def triple_cut
+        a = @deck.index( "A" )
+	b = @deck.index( "B" )
+	a,b = b,a if a > b
+	@deck.replace( [@deck[(b+1)..-1],
+		        @deck[a..b],
+			@deck[0..(a-1)]].flatten )
+    end
+
+    def count_cut
+        a = @deck.last
+	a = 53 if a.instance_of? String
+	
+	temp = @deck[0..(a-1)]
+	@deck[0..(a-1)] = []
+	@deck[-1..-1] = [temp, @deck.last].flatten
+    end
+
+    # ++++++++++++++++++++
+    # O U T P U T   A P I
+    # ++++++++++++++++++++ 
+    def output_letter
+        a = @deck.first
+	a = 53 if a.instance_of? String
+	output = @deck[a]
+	if output.instance_of? String
+	    nil
+	else
+	    output -= 26 if output > 26
+	    (output + 64).chr
+	end
+    end	
+
+    # +++++++++++++++++ 
+    # T E S T  A P I s
+    # +++++++++++++++++ 
+    def card_at(index)
+        @deck[index]
+    end
+
+    def deck_size
+        @deck.length
+    end
 end
 
 # ==========================================
@@ -144,3 +241,23 @@ OPTIONS = {
     :encode => nil,
     :decode => nil
 }
+
+if __FILE__ == $0
+    ARGV.options do |o|
+        # parse the command line options
+	o.separator ""
+	o.on("-e", "--encode=[value]", String, "Encrypt a plaintext message" ) { |OPTIONS[:encode]| }
+	o.on("-d", "--decode=[value]", String, "Decrypt an encoded message" ) { |OPTIONS[:decode]| }
+	o.separator ""
+	o.parse!
+
+	# run the actual script
+	if OPTIONS[:encode] != "" and OPTIONS[:encode] != nil
+	    puts Cipher.new.encode(OPTIONS[:encode])
+     	elsif OPTIONS[:decode] != "" and OPTIONS[:decode] != nil
+	    puts Cipher.new.decode(OPTIONS[:decode])
+	else
+	    puts o
+	end
+    end
+end
